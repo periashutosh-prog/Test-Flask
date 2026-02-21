093ac0641aaa7dad0f77ed800625dc1f8c5f88ec
periashutosh-prog <periashutosh@gmail.com>
2026-02-21 18:06:24 +0530

    Basic flask tester

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def home():
     name = request.args.get("name", "World")
-    return jsonify({
-        "message": f"Hello {name}"
-    })
+    return jsonify({"message": f"Hello {name}"})
 
-# Required for Vercel
-def handler(request, context):
-    return app(request.environ, lambda *args: None)
+# This is REQUIRED for Vercel
+app = app
